5cc126173cfe1c48df225a4273321825044a3859
JordanFinatto <jordan.finatto@universo.univates.br>
2024-05-17 20:47:10 -0300

    melhorias na tela de Metas e Investimentos

--- lib/metas.dart
+++ lib/metas.dart
@@ -18,12 +18,19 @@ class _MetasState extends State<Metas> {
   final FocusNode _valorFocusNode = FocusNode();
   String? _tipoPeriodoSelecionado;
   int _quantidadeMetas = 0;
+  final NumberFormat _currencyFormat =
+      NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
 
   @override
   void initState() {
     super.initState();
     _tipoPeriodoSelecionado = 'anos';
     _contarMetas();
+  }
+
+  String formatCurrency(String value) {
+    final cleanValue = value.replaceAll(RegExp(r'\D'), '');
+    return _currencyFormat.format(double.parse(cleanValue) / 100);
   }
 
   Future<void> _contarMetas() async {
@@ -39,7 +46,11 @@ class _MetasState extends State<Metas> {
 
   Future<void> _salvarMetas() async {
     final String periodo = _periodoController.text;
-    final double valor = double.tryParse(_valorController.text) ?? 0.0;
+    final double valor = double.tryParse(_valorController.text
+            .replaceAll('.', '')
+            .replaceAll(',', '.')
+            .replaceAll('R\$', '')) ??
+        0.0;
     final String tipoPeriodo = _tipoPeriodoSelecionado ?? 'anos';
 
     if (periodo.isNotEmpty && valor > 0) {
@@ -108,12 +119,10 @@ class _MetasState extends State<Metas> {
         _valorController.clear();
 
         _contarMetas(); // Atualiza o contador de metas
-
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content:
-            Text('Erro ao salvar a meta. Tente novamente mais tarde.'),
+            content: Text('Erro ao salvar a meta. Tente novamente mais tarde.'),
             duration: Duration(seconds: 2),
           ),
         );
@@ -131,7 +140,7 @@ class _MetasState extends State<Metas> {
   void _limparCampos() {
     setState(() {
       _periodoController.clear();
-      _valorController.clear();
+      _valorController.text = 'R\$ 0,00';
       _tipoPeriodoSelecionado = 'anos';
     });
   }
@@ -173,18 +182,53 @@ class _MetasState extends State<Metas> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Metas Financeiras'),
+        title: Text('Metas e Investimentos'),
         backgroundColor: Colors.green.shade800,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+      body: DefaultTabController(
+        length: 2,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Row(
-              children: [
-                Expanded(
+            Container(
+              color: Colors.green.shade800, // Cor de fundo das abas
+              child: TabBar(
+                labelStyle: TextStyle(fontSize: 18), // Aumenta o tamanho da fonte das Tabs em 3 unidades
+                tabs: [
+                  Tab(text: 'Metas'),
+                  Tab(text: 'Investimentos'),
+                ],
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  _buildMetasTab(),
+                  _buildInvestimentosTab(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMetasTab() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Conteúdo da aba "Metas" aqui
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            // Alinha no final (inferior)
+            children: [
+              Expanded(
+                child: Container(
+                  height: 70,
+                  // Altura para corresponder à altura do DropdownButtonFormField
                   child: TextFormField(
                     controller: _periodoController,
                     decoration: InputDecoration(
@@ -194,13 +238,21 @@ class _MetasState extends State<Metas> {
                     inputFormatters: <TextInputFormatter>[
                       FilteringTextInputFormatter.digitsOnly,
                     ],
+                    style: TextStyle(fontSize: 18),
+                    // Aumenta o tamanho da fonte
                     onFieldSubmitted: (value) {
                       FocusScope.of(context).requestFocus(_valorFocusNode);
                     },
                   ),
                 ),
-                SizedBox(width: 10),
-                DropdownButton<String>(
+              ),
+              SizedBox(width: 10),
+              Container(
+                height: 56,
+                // Altura para corresponder à altura do TextFormField
+                width: 150,
+                // Defina a largura desejada
+                child: DropdownButtonFormField<String>(
                   value: _tipoPeriodoSelecionado,
                   onChanged: (String? newValue) {
                     setState(() {
@@ -211,145 +263,192 @@ class _MetasState extends State<Metas> {
                       .map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
-                      child: Text(value),
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                            fontSize:
+                                18), // Aumente o tamanho da fonte, se necessário
+                      ),
                     );
                   }).toList(),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              controller: _valorController,
-              focusNode: _valorFocusNode,
-              decoration: InputDecoration(
-                labelText: 'Valor',
               ),
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ],
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            controller: _valorController,
+            focusNode: _valorFocusNode,
+            decoration: InputDecoration(
+              labelText: 'Valor',
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Ink(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color(0xFF1000D3), Color(0xFF1A6BFF)], // Cores base do gradiente
-                      begin: Alignment.topCenter, // Início do gradiente (cima)
-                      end: Alignment.bottomCenter, // Fim do gradiente (baixo)
-                    ),
-                    borderRadius: BorderRadius.circular(8), // Adicione bordas arredondadas conforme necessário
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            onChanged: (value) {
+              final previousCursorPosition =
+                  _valorController.selection.baseOffset;
+              final formatted = formatCurrency(value);
+              final lengthDifference =
+                  formatted.length - _valorController.text.length;
+              final newCursorPosition =
+                  (previousCursorPosition + lengthDifference) < 0
+                      ? 0
+                      : (previousCursorPosition + lengthDifference);
+              setState(() {
+                _valorController.value = TextEditingValue(
+                  text: formatted,
+                  selection: TextSelection.collapsed(offset: newCursorPosition),
+                );
+              });
+            },
+            style: TextStyle(fontSize: 18), // Aumenta o tamanho da fonte
+          ),
+
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Ink(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF1000D3), Color(0xFF1A6BFF)],
+                    // Cores base do gradiente
+                    begin: Alignment.topCenter,
+                    // Início do gradiente (cima)
+                    end: Alignment.bottomCenter, // Fim do gradiente (baixo)
                   ),
-                  child: InkWell(
-                    onTap: _salvarMetas, // Chame a função _salvarMetas ao clicar
-                    child: Container(
-                      width: 170, // Ajuste a largura conforme necessário
-                      height: 45, // Ajuste a altura conforme necessário
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Salvar meta',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white, // Cor do texto branca para melhor contraste
-                        ),
+                  borderRadius: BorderRadius.circular(
+                      8), // Adicione bordas arredondadas conforme necessário
+                ),
+                child: InkWell(
+                  onTap: _salvarMetas,
+                  // Chame a função _salvarMetas ao clicar
+                  child: Container(
+                    width: 150, // Ajuste a largura conforme necessário
+                    height: 45, // Ajuste a altura conforme necessário
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Salvar meta',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors
+                            .white, // Cor do texto branca para melhor contraste
                       ),
                     ),
                   ),
                 ),
-                Ink(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color(0xFF960000), Color(0xFFFF2828)], // Cores base do gradiente
-                      begin: Alignment.topCenter, // Início do gradiente (cima)
-                      end: Alignment.bottomCenter, // Fim do gradiente (baixo)
-                    ),
-                    borderRadius: BorderRadius.circular(8), // Adicione bordas arredondadas conforme necessário
+              ),
+              Ink(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF960000), Color(0xFFFF2828)],
+                    // Cores base do gradiente
+                    begin: Alignment.topCenter,
+                    // Início do gradiente (cima)
+                    end: Alignment.bottomCenter, // Fim do gradiente (baixo)
                   ),
-                  child: InkWell(
-                    onTap: _limparCampos, // Chame a função _limparCampos ao clicar
-                    child: Container(
-                      width: 170, // Ajuste a largura conforme necessário
-                      height: 45, // Ajuste a altura conforme necessário
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Limpar campos',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white, // Cor do texto branca para melhor contraste
-                        ),
+                  borderRadius: BorderRadius.circular(
+                      8), // Adicione bordas arredondadas conforme necessário
+                ),
+                child: InkWell(
+                  onTap: _limparCampos,
+                  // Chame a função _limparCampos ao clicar
+                  child: Container(
+                    width: 150, // Ajuste a largura conforme necessário
+                    height: 45, // Ajuste a altura conforme necessário
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Limpar campos',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors
+                            .white, // Cor do texto branca para melhor contraste
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Text('Suas metas:',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                SizedBox(width: 10),
-                Text(
-                  '$_quantidadeMetas',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.green),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: FutureBuilder<QuerySnapshot>(
-                future: FirebaseFirestore.instance
-                    .collection('metas')
-                    .where('email', isEqualTo: widget.email)
-                    .get(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(child: CircularProgressIndicator());
-                  }
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Text('Suas metas:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(width: 10),
+              Text(
+                '$_quantidadeMetas',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: FutureBuilder<QuerySnapshot>(
+              future: FirebaseFirestore.instance
+                  .collection('metas')
+                  .where('email', isEqualTo: widget.email)
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                final List<QueryDocumentSnapshot> metas = snapshot.data!.docs;
 
-                  final List<QueryDocumentSnapshot> metas =
-                      snapshot.data!.docs;
+                if (metas.isEmpty) {
+                  return Center(child: Text('Nenhuma meta encontrada.'));
+                }
 
-                  if (metas.isEmpty) {
-                    return Center(child: Text('Nenhuma meta encontrada.'));
-                  }
+                return ListView.builder(
+                  itemCount: metas.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final Map<String, dynamic> data =
+                        metas[index].data() as Map<String, dynamic>;
+                    final Timestamp timestamp =
+                        data['datafimparameta'] as Timestamp;
+                    final DateTime dataFim = timestamp.toDate();
+                    final String dataFimFormatada =
+                        DateFormat('dd/MM/yyyy').format(dataFim);
+                    final NumberFormat formatter =
+                        NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
+                    final String valorFormatado =
+                        formatter.format(data['valor']);
+                    final String id =
+                        metas[index].id; // Obter o ID do documento
 
-                  return ListView.builder(
-                    itemCount: metas.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final Map<String, dynamic> data = metas[index].data() as Map<String, dynamic>;
-                      final Timestamp timestamp = data['datafimparameta'] as Timestamp;
-                      final DateTime dataFim = timestamp.toDate();
-                      final String dataFimFormatada = DateFormat('dd/MM/yyyy').format(dataFim);
-                      final NumberFormat formatter = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
-                      final String valorFormatado = formatter.format(data['valor']);
-                      final String id = metas[index].id; // Obter o ID do documento
-
-                      return Column(
-                        children: [
-                          ListTile(
-                            title: Text(
-                              'Em ${data['periodo']} ${data['tipoperiodo']}, valor de $valorFormatado\nData fim: $dataFimFormatada',
-                            ),
-                            trailing: IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () {
-                                _excluirMeta(id);
-                              },
-                            ),
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            'Em ${data['periodo']} ${data['tipoperiodo']}, valor de $valorFormatado\nData fim: $dataFimFormatada',
                           ),
-                          if (index != metas.length - 1) Divider(),
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              _excluirMeta(id);
+                            },
+                          ),
+                        ),
+                        if (index != metas.length - 1) Divider(),
+                      ],
+                    );
+                  },
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildInvestimentosTab() {
+    return Center(
+      child: Text('Conteúdo da aba "Investimentos"'),
     );
   }
 }
